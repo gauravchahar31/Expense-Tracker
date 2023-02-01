d8cb93321b717affb11e9964ace0c82499018b0a
Gaurav Kumar <76642994+gauravchahar31@users.noreply.github.com>
2023-02-02 00:29:56 +0530

    deployment

--- main.js
+++ main.js
@@ -20,7 +20,19 @@ const purchaseRoutes = require('./routes/purchase');
 const leaderboardRoutes = require('./routes/leaderboard');
 
 const cors = require('cors');
-app.use(cors());
+const corsOpts = {
+    origin: '*',
+  
+    methods: [
+      'GET',
+      'POST',
+    ],
+  
+    allowedHeaders: [
+      'Content-Type',
+    ],
+  };
+app.use(cors(corsOpts));
 
 // app.use(bodyParser.urlencoded({extended: true}));
 require('dotenv').config()
@@ -29,11 +41,6 @@ app.use(compression());
 app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cookieParser());
-
-app.use((req, res, next) => {
-    res.sendFile(path.join(__dirname, `views/${req.url}`));
-    next();
-});
 
 app.use(async (req, res, next) => {
     if(req.cookies.user){
@@ -51,6 +58,11 @@ app.use('/expense', expenseRoutes);
 app.use('/purchase', purchaseRoutes);
 app.use('/leaderboard', leaderboardRoutes);
 app.use('/', homeRoutes);
+
+app.use((req, res, next) => {
+    res.sendFile(path.join(__dirname, `views/${req.url}`));
+    // next();
+});
 
 Expense.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Expense);
