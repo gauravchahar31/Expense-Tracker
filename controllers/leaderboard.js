e462e574d0ea8b26208a70681d296f9792b26b31
Gaurav Kumar <76642994+gauravchahar31@users.noreply.github.com>
2023-02-02 02:46:03 +0530

    resolved sequelize error

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -5,14 +5,14 @@ const Expense = require('../models/Expense');
 exports.getLeaderboard = async (req, res) => {
     try{
         const leaderboard = await User.findAll({
-            attributes: ['id', 'name',[sequelize.fn('sum', sequelize.col('expenses.amount')), 'total_cost']],
+            attributes: ['id', 'name',[sequelize.fn('sum', sequelize.col('Expenses.amount')), 'total_cost']],
             include: [
                 {
                     model: Expense,
                     attributes: []
                 }
             ],
-            group:['user.id'],
+            group:['User.id'],
             order:[['total_cost', 'DESC']]
         })
         res.json(leaderboard);
